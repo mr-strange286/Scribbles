d29c23d88e0f8ea7a7cd7ef8f30713003c2d57b1
itsrohit000 <rahana.rohit000@gmail.com>
2026-07-21 08:18:50 +0530

    Redesign Scribbles homepage v2

--- js/script.js
+++ js/script.js
@@ -29,3 +29,39 @@ sections.forEach(section => {
     observer.observe(section);
 
 });
+
+const counters = [
+
+    { id: "storiesCount", value: 78 },
+
+    { id: "poemsCount", value: 24 },
+
+    { id: "writersCount", value: 150 },
+
+    { id: "eventsCount", value: 18 }
+
+];
+
+counters.forEach(counter => {
+
+    let current = 0;
+
+    const element = document.getElementById(counter.id);
+
+    if (!element) return;
+
+    const interval = setInterval(() => {
+
+        current++;
+
+        element.textContent = current;
+
+        if (current >= counter.value) {
+
+            clearInterval(interval);
+
+        }
+
+    }, 15);
+
+}); 
